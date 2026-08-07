3b4d5d971566e74b5f715be43ab7a438c8c32a7f
Irati San Sebastian <irati22s@gmail.com>
2026-08-07 14:05:11 +0200

    Crear página de revistas

--- src/app/revistas/page.tsx
+++ src/app/revistas/page.tsx
@@ -1,7 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./page.module.css";
+
+const revistas = [
+  {
+    id: "edicion-1",
+    nombre: "Edición 1",
+    imagen: "/photos/cover01.webp",
+  },
+  {
+    id: "edicion-2",
+    nombre: "Edición 2",
+    imagen: "/photos/cover02.webp",
+  },
+  {
+    id: "edicion-3",
+    nombre: "Edición 3",
+    imagen: "/photos/cover03.webp",
+  },
+  {
+    id: "edicion-4",
+    nombre: "Edición 4",
+    imagen: "/photos/cover04.webp",
+  },
+];
+
 export default function Revistas() {
   return (
-    <main>
-      <h1>Revistas</h1>
+    <main className={styles.revistaPage}>
+      <div className={styles.grid}>
+        {revistas.map((revista) => (
+          <Link
+            key={revista.id}
+            href={`/revistas/${revista.id}`}
+            className={styles.revista}
+          >
+            <div className={styles.portada}>
+              <Image
+                src={revista.imagen}
+                alt={revista.nombre}
+                fill
+                sizes="(max-width: 768px) 70vw, (max-width: 1024px) 40vw, 25vw"
+                className={styles.imagen}
+              />
+            </div>
+
+            <p className={styles.nombre}>{revista.nombre}</p>
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
